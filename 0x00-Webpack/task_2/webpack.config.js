ef213ba75643ae108d9c8c724ac42b896718ccb3
Cha1mae <chaimastaifa@gmail.com>
2024-05-20 10:04:56 -0700

    check

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,38 +1,34 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production",
-  entry: {
-    main: path.resolve(__dirname, "./js/dashboard_main.js"),
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
   },
   output: {
-    path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
-  },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
+    path: path.resolve(__dirname, "public")
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(gif|png|jp?g|svg)$/i,
-        use: [
-          "file-loader",
+        test: /\.(png|jpe?g|gif)$/i,
+        use: ['file-loader',
           {
-            loader: "image-webpack-loader",
+            loader: 'image-webpack-loader',
             options: {
-              bypassOnDebug: true,
-              disable: true,
+                bypassOnDebug: true,
+                disable: true,
             },
           },
         ],
       },
-    ],
-  },
+    ]
+  }
 };
